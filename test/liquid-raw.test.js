9393083da1c98807a0c3d788a18ddffce416ae11
Julian Gruber <julian@juliangruber.com>
2019-09-20 10:28:31 +0200

    import liquid-raw test

---
+++ test/liquid-raw.test.js
@@ -0,0 +1,49 @@
+const { obfuscate, deobfuscate } = require('../liquid-raw')
+const { test } = require('tap')
+
+test('liquidRaw module', async t => {
+  await t.test('obfuscate(template)', async t => {
+    await t.test('works on inline raw templates', async t => {
+      const input = `
+        For example: {% raw %} {% include cool_header.html %} {% endraw %}.
+      `
+      const output = `For example: {---% raw %---} {---% include cool_header.html %---} {---% endraw %---}.`
+      t.equal(obfuscate(input), output)
+    })
+
+    await t.test('works on multiline raw templates', async t => {
+      const input = `{% raw %}
+  {% include cool_header.html %}
+  {{ foo.bar.baz }}
+{% endraw %}`
+
+      const output = `{---% raw %---}
+  {---% include cool_header.html %---}
+  {---{ foo.bar.baz }---}
+{---% endraw %---}`
+      t.equal(obfuscate(input), output)
+    })
+  })
+
+  await t.test('deobfuscate(template)', async t => {
+    await t.test('works on inline/one-liner raw templates', async t => {
+      const input = `For example: {---% raw %---} {---% include cool_header.html %---} {---% endraw %---}.`
+      const output = `For example: {% raw %} {% include cool_header.html %} {% endraw %}.`
+      t.equal(deobfuscate(input), output)
+    })
+
+    await t.test('works on multiline raw templates', async t => {
+      const input = `{---% raw %---}
+  {---% include cool_header.html %---}
+  {---{ foo.bar.baz }---}
+{---% endraw %---}`
+
+      const output = `{% raw %}
+  {% include cool_header.html %}
+  {{ foo.bar.baz }}
+{% endraw %}`
+
+      t.equal(deobfuscate(input), output)
+    })
+  })
+})
