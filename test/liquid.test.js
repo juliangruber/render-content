2aea090a4fc255b6f4a711d663c229b02ff03638
Julian Gruber <julian@juliangruber.com>
2019-09-20 10:26:10 +0200

    import liquid test

---
+++ test/liquid.test.js
@@ -0,0 +1,45 @@
+const liquid = require('../liquid')
+const { test } = require('tap')
+
+const template = `
+  {% if productVersion ver_gt "2.13" %}up to date{% endif %}
+  {% if productVersion ver_lt "2.13" %}out of date{% endif %}
+`
+
+test('liquid template parser', async t => {
+  await t.test('custom operators', async t => {
+    await t.test('ver_gt', async t => {
+      await t.test('works as expected', async t => {
+        const context = {
+          productVersion: '2.14'
+        }
+        const output = await liquid.parseAndRender(template, context)
+        t.equal(output.trim(), 'up to date')
+      })
+
+      await t.test('returns false when given value is not numeric, like `dotcom`', async t => {
+        const context = {
+          productVersion: 'dotcom'
+        }
+        const output = await liquid.parseAndRender(template, context)
+        t.equal(output.trim(), '')
+      })
+
+      await t.test('returns false when given value is falsy', async t => {
+        const context = {}
+        const output = await liquid.parseAndRender(template, context)
+        t.equal(output.trim(), '')
+      })
+    })
+
+    await t.test('ver_lt', async t => {
+      await t.test('works as expected', async t => {
+        const context = {
+          productVersion: '2.12'
+        }
+        const output = await liquid.parseAndRender(template, context)
+        t.equal(output.trim(), 'out of date')
+      })
+    })
+  })
+})
